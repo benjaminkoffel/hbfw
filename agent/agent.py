8a470198e4d1844cf6eb0e028d79ceb00d70ab76
Benjamin Koffel <benjaminkoffel@gmail.com>
2018-08-26 21:43:04 +1000

    update rules

--- agent/agent.py
+++ agent/agent.py
@@ -7,22 +7,8 @@ import requests
 import yaml
 
 logging.basicConfig(level=logging.INFO)
-
-def update_iptables6():
-    table = iptc.Table6(iptc.Table6.FILTER)
-    table.autocommit = False
-    chain = iptc.Chain(table, 'INPUT')
-    old_rules = chain.rules
-    for rule in old_rules:
-        chain.delete_rule(rule)
-    rule = iptc.Rule6()
-    rule.target = iptc.Target(rule, 'DROP')
-    chain.insert_rule(rule)
-    table.commit()
-    table.refresh()
     
-def write_chain(name, rules):
-    table = iptc.Table(iptc.Table.FILTER)
+def write_chain(table, name, rules):
     table.autocommit = False
     chain = iptc.Chain(table, name)
     old_rules = chain.rules
@@ -33,44 +19,37 @@ def write_chain(name, rules):
     table.commit()
     table.refresh()
 
-def accept_localhost():
-    rule = iptc.Rule()
+def accept_localhost(rule):
     rule.target = iptc.Target(rule, 'ACCEPT')
     rule.in_interface = 'lo'
     return rule
 
-def accept_related_established():
-    rule = iptc.Rule()
+def accept_related_established(rule):
     rule.target = iptc.Target(rule, 'ACCEPT')
     match = iptc.Match(rule, 'state')
     match.state = 'RELATED,ESTABLISHED'
     rule.add_match(match)
     return rule
 
-def drop_all():
-    rule = iptc.Rule()
-    rule.target = iptc.Target(rule, 'DROP')
-    return rule
-
-def create_rule(ip, protocol, port):
-    rule = iptc.Rule()
-    rule.target = iptc.Target(rule, 'ACCEPT')
-    rule.src = ip
-    rule.protocol = protocol
-    match = rule.create_match(protocol)
-    match.dport = port
-    rule.add_match(match)
+def create_rule(rule, action, ip, protocol, port):
+    rule.target = iptc.Target(rule, action)
+    if ip:
+        rule.src = ip
+    if protocol:
+        rule.protocol = protocol
+        if port:
+            match = rule.create_match(protocol)
+            match.dport = port
+            rule.add_match(match)
     return rule
 
 def update_iptables(policy):
-    rules = []
-    rules.append(accept_localhost())
-    rules.append(accept_related_established())
-    for p in policy:
-        v = p.split()
-        rules.append(create_rule(v[0], v[1], v[2]))
-    rules.append(drop_all())
-    write_chain('INPUT', rules)
+    policies = [p.split() for p in policy]
+    for chain in ['INPUT', 'OUTPUT', 'FORWARD']:
+        rules = [accept_localhost(iptc.Rule6()), accept_related_established(iptc.Rule6())]
+        rules += [create_rule(iptc.Rule(), p[1], p[2], p[3], p[4]) if len(p) > 2 else create_rule(iptc.Rule(), p[1], None, None, None) for p in policies where p[0] == chain]
+        write_chain(iptc.Table(iptc.Table.FILTER), chain, rules)
+        write_chain(iptc.Table6(iptc.Table6.FILTER), chain, [create_rule(iptc.Rule6(), 'DROP', None, None, None)])
 
 def poll(interval, uri, token):
     while True:
@@ -79,7 +58,6 @@ def poll(interval, uri, token):
             response = requests.get(uri, headers=headers)
             policy = response.json()
             update_iptables(policy)
-            update_iptables6()
             logging.info('%d %s', response.status_code, policy)
         except Exception as e:
             logging.error(e)
